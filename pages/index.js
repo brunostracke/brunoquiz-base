e4c2722691d4209951c9267529748dc6a7abbbfd
brunostracke <brunostracke@gmail.com>
2021-01-25 16:22:02 -0300

    codigo ao final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Footer from '../src/components/Footer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    paddin: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>
+              Quiz da Britney
+            </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, laboriosam provident. Animi dolores molestiae suscipit consequatur praesentium, ut molestias accusantium. Accusantium voluptas fugiat et deserunt totam ducimus ut omnis possimus.
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>
+              Quizes do povo
+            </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, laboriosam provident. Animi dolores molestiae suscipit consequatur praesentium, ut molestias accusantium. Accusantium voluptas fugiat et deserunt totam ducimus ut omnis possimus.
+            </p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+        <GitHubCorner projectUrl="https://github.com/brunostracke"/>
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
