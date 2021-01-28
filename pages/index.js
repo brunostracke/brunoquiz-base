15e78d06751bb692e780c2b43ada77cd6deb7f69
brunostracke <brunostracke@gmail.com>
2021-01-28 19:42:42 -0300

    aula 2 e 3

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,24 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Footer from '../src/components/Footer'
-import Head from 'next/head'
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    paddin: 15px;
-  }
-`;
-
+/* eslint-disable max-len */
+import React from 'react';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import Logo from '../src/components/Logo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-    <div>
-    <Head>
-      <title>Quizney</title>
-      <link rel="icon" href={db.icon}></link>
-    </Head>
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <Logo />
         <Widget>
           <Widget.Header>
             <h1>
@@ -41,9 +26,21 @@ export default function Home() {
             </h1>
           </Widget.Header>
           <Widget.Content>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, laboriosam provident. Animi dolores molestiae suscipit consequatur praesentium, ut molestias accusantium. Accusantium voluptas fugiat et deserunt totam ducimus ut omnis possimus.
-            </p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz aí teu nomeney"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -61,8 +58,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/brunostracke"/>
+      <GitHubCorner projectUrl="https://github.com/brunostracke" />
     </QuizBackground>
-    </div>
-  )
+  );
 }
